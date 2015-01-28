7ac877690d6d18688e5e527965295d595dab30df
greg <greg@asana.com>
2015-01-27 22:34:02 -0800

    Added tags

--- test/resource_spec.js
+++ test/resource_spec.js
@@ -10,7 +10,7 @@ describe('Resource', function() {
   describe('#names', function() {
     it('should return known resources in order', function() {
       assert.deepEqual(resource.names(), [
-        'project', 'story', 'task', 'team', 'user', 'workspace'
+        'project', 'story', 'tag', 'task', 'team', 'user', 'workspace'
       ]);
     });
   });
